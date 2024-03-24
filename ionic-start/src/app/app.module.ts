2fbe914bda323daadfd52e6a15468dc3a1db11d9
Brayan Martinez <142542930+BrayanMtz314@users.noreply.github.com>
2024-03-23 20:22:03 -0600

    Practica 7 log in

--- ionic-start/src/app/app.module.ts
+++ ionic-start/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FloatMenuComponent } from './componentes/float-menu/float-menu.component';
+
 
 @NgModule({
   declarations: [AppComponent, FloatMenuComponent],
--- ionic-start/src/environments/environment.prod.ts
+++ ionic-start/src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyCROl_fQmSPfHqwVJ23MBDqxarmDHp4ih8",
-    authDomain: "ionic-start-79e7e.firebaseapp.com",
-    projectId: "ionic-start-79e7e",
-    storageBucket: "ionic-start-79e7e.appspot.com",
-    messagingSenderId: "28806612194",
-    appId: "1:28806612194:web:de523ff091ca59b3414abb",
-    measurementId: "G-LB3BXX518Z"
-  }
+
 };
